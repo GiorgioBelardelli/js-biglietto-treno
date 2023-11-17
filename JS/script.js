5522c33155a1d5b175f2516cfa50922f7cc05d06
GiorgioBelardelli <asterapocalypse@gmail.com>
2023-11-17 15:45:55 +0100

    tolto il debug

--- JS/script.js
+++ JS/script.js
@@ -6,38 +6,26 @@ va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
 L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 Questo richiederà un minimo di ricerca.
-
 */
 
-const distance = parseInt(prompt("Inserisci il la distanza (in KM) che devi percorrere"));
+let ticketPrice = parseFloat(distance * 0.21);
 
+const distance = parseInt(prompt("Inserisci il la distanza (in KM) che devi percorrere"));
 const passengerAge = parseInt(prompt("Inserisci la tua eta'"));
 
-let ticketPrice = parseFloat(distance * 0.21);
-console.log(ticketPrice);
+
 
 if ( passengerAge < 18 ){
 
     const juniorSale = 0.80 ;
-
     ticketPrice = ticketPrice * juniorSale;
-
-    (console.log(ticketPrice));
 
 } else if ( passengerAge > 65){
 
     const elderSale = 0.60;
-
     ticketPrice = ticketPrice * elderSale;
-
-    (console.log(ticketPrice));
 
 }
 
 document.getElementById("tot-prezzo").innerHTML = "Il costo del tuo biglietto e' di: " + ticketPrice + " euro"
 
-//se il l'eta del passeggero é minore di 20 allora il prezzo va moltiplicato per 0.80
-
-//se l 'eta del passeggero é maggiore di 65 allora il prezzo va moltiplicato per 0.60
-
-//altrimenti rimane la tariffa normale
